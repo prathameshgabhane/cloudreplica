fa8203a89ac4c7e7f033b0f3d6da091d01dfd0d2
sourish-das <162016275+sourish-das@users.noreply.github.com>
2026-02-10 13:31:37 +0100

    Update aws.js

--- scripts/lib/aws.js
+++ scripts/lib/aws.js
@@ -1,2 +1,8 @@
-// Placeholder - aws helpers will be added after structure is ready
-module.exports = {};
+// scripts/lib/aws.js
+function isWantedEc2Family(instance = "") {
+  // m c r t x i z h
+  const c = String(instance)[0]?.toLowerCase();
+  return ["m","c","r","t","x","i","z","h"].includes(c);
+}
+
+module.exports = { isWantedEc2Family };
